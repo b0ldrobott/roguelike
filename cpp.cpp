e915290d3eaf617c7c319a3ee49bf95e2a043445
b0ldrobott <erin.gwc@gmail.com>
2021-04-22 17:09:59 +0900

    added win and gameover feature

--- cpp.cpp
+++ cpp.cpp
@@ -6,8 +6,8 @@ const int n = 5;
 char map[n][n + 1] = {
 	"#####",
 	"#   #",
-	"#   #",
-	"#   #",
+	"# Y #",
+	"#M  #",
 	"#####"
 };
 
@@ -41,18 +41,59 @@ int main()
 		print();
 
 		if (GetAsyncKeyState(VK_UP) & 0x8001) {
-			nowY--;
+			if (map[nowY - 1][nowX] != '#') {
+				nowY--;
+			}
+
 		}
 		if (GetAsyncKeyState(VK_DOWN) & 0x8001) {
-			nowY++;
+			if (map[nowY + 1][nowX] != '#') {
+				nowY++;
+			}
 		}
 		if (GetAsyncKeyState(VK_LEFT) & 0x8001) {
-			nowX--;
+			if (map[nowY][nowX - 1] != '#') {
+				nowX--;
+			}
 		}
 		if (GetAsyncKeyState(VK_RIGHT) & 0x8001) {
-			nowX++;
+			if (map[nowY][nowX + 1] != '#') {
+				nowX++;
+			}
 		}
 		Sleep(100);
+
+		if (map[nowY][nowX] == 'Y') {
+			print();
+			for (int i = 0; i < 5; i++) {
+				Sleep(200);
+				system("color c4");
+				Sleep(200);
+				system("color 3f");
+				Sleep(300);
+			}
+			system("cls");
+			Sleep(500);
+			cout << "\n\n\n\n\n WIN!\n\n\n\n\n";
+			Sleep(500);
+			break;
+		}
+
+		if (map[nowY][nowX] == 'M') {
+			print();
+			for (int i = 0; i < 5; i++) {
+				Sleep(200);
+				system("color c4");
+				Sleep(200);
+				system("color 3f");
+				Sleep(300);
+			}
+			system("cls");
+			Sleep(500);
+			cout << "\n\n\n\n\n GAME OVER!\n\n\n\n\n";
+			Sleep(500);
+			break;
+		}
 	}
 	return 0;
 }
